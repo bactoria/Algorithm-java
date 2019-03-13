1cb68029bd6e0799145358d9402a8ad8d931ab29
bactoria <bactoria@gmail.com>
2019-03-13 23:59:59 +0900

    6549(SegmentTree, DiviedConquer) 추가 기존의 코드는 시간초과였음.

--- SegmentTree/_6549.java
+++ SegmentTree/_6549.java
@@ -1,100 +1,78 @@
-package SegmentTree;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * @author : Juno Hwang (sbukkk)
- * @date   : 2018. 2. 10.
- *  https://github.com/bactoria/BOJ/
- *  https://www.acmicpc.net/problem/6549
- *  
- */
-public class _6549 {
-	private static int[] height;
+public class Main {
 
-	static class Node{
-		int width;
-		int minH;
+    static int N;
+    static int HEIGHT;
+    static int[] segmentTree;
+    static int LEFT;
+    static int RIGHT;
+    static int[] values;
+    static int leafStart;
 
-		public Node(int width, int minH) {
-			this.width = width;
-			this.minH = minH;
-		}
+    public static void main(String[] args) throws IOException {
 
-	}
-	
-	static class SegmentTree {
-		Node[] tree;
-		
-		SegmentTree(int n){
-			final int TREE_HEIGHT = (int)Math.ceil(Math.log10(n) / Math.log10(2));
-			tree = new Node[2<<TREE_HEIGHT];
-		}
-		
-		public Node init(int index, int start, int end) {
-			
-			if(start == end) return tree[index] = new Node(1,height[start]);
-			
-			Node leftN = init(index*2, start, (start+end) / 2 );
-			Node rightN = init(index*2+1 , (start+end) / 2 + 1 , end );
-			
-			int rootWidth = leftN.width + rightN.width;
-			int rootMinH = Math.min ( leftN.minH , rightN.minH );
-			return tree[index] = new Node (  rootWidth , rootMinH );
-		}
-		
-		
-		public Node findMaxArea(int index, int start, int end, int searchL, int searchR) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			if(searchR < start || end < searchL) 	return new Node(0, Integer.MAX_VALUE);
-			else if(searchL <= start && end <= searchR ) return tree[index];
-			
-			Node leftN = findMaxArea(index*2, start, (start+end)/2, searchL, searchR);
-			Node rightN = findMaxArea(index*2+1, (start+end)/2+1, end, searchL, searchR);
-			
-			int rootWidth = leftN.width + rightN.width;
-			int rootMinH = Math.min ( leftN.minH , rightN.minH );
+        while (true) {
 
-			return new Node(rootWidth, rootMinH);
-		}
-	}
-	
-	
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		while(true) {
-			//st = new StringTokenizer(br.readLine(), " ");
-			//int n = Integer.parseInt(st.nextToken());
-			int n = 100000;
-			if(n == 0) break;
-			
-			height = new int[n+1];
-			for (int i = 1; i <= n; i++) {
-				//height[i] = Integer.parseInt(st.nextToken());
-				height[i] = i;
-			}
-			
-			SegmentTree sTree = new SegmentTree(n);
-			//tree �ʱ�ȭ
-			sTree.init(1, 1, n);
-			
-			long maxV = 0;
-			Node tempN;
-			for (int i = 1; i <= n; i++) {
-				for (int j = i; j <= n; j++) {
-					tempN = sTree.findMaxArea(1,1,n,i,j);
-					maxV = Math.max(maxV , (long)tempN.minH * tempN.width);
-				}
-			}
-			
-			sb.append(maxV+"\n");	
-		}
-		System.out.println(sb.toString());
-	}
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            N = Integer.parseInt(st.nextToken());
+            if (N == 0) break;
+
+            HEIGHT = (int) Math.ceil(Math.log(N) / Math.log(2)) + 1;
+            leafStart = (int) (Math.pow(2, HEIGHT - 1) - 1);
+            int leafEnd = leafStart + N - 1;
+
+            segmentTree = new int[leafEnd + 1];
+
+            values = new int[N + 1];
+            values[0] = Integer.MAX_VALUE;
+            for (int i = 1; i <= N; i++) values[i] = Integer.parseInt(st.nextToken());
+
+
+            for (int i = 1; i <= N; i++) {
+                init(0, 1, leafStart + 1, i);
+            }
+
+            System.out.println(process(1, N));
+        }
+    }
+
+    private static void init(int index, int start, int end, int leaf) {
+        segmentTree[index] = values[segmentTree[index]] > values[leaf] ? leaf : segmentTree[index];
+
+        if (start == end) segmentTree[index] = leaf;
+        else if (leaf <= (start + end) / 2) init(index * 2 + 1, start, (start + end) / 2, leaf); // 좌
+        else init(index * 2 + 2, (start + end) / 2 + 1, end, leaf); // 우
+    }
+
+    private static long process(int left, int right) {
+        if (left > right) return 0;
+        else if (left == right) return values[left];
+
+        LEFT = left;
+        RIGHT = right;
+        int minValue = findMin(0, 1, leafStart + 1);
+        return max((long) (right - left + 1) * values[minValue], process(left, minValue - 1), process(minValue + 1, right));
+    }
+
+    private static int findMin(int index, int start, int end) {
+        if (start > RIGHT || LEFT > end) return 0;
+        else if (LEFT <= start && RIGHT >= end) return segmentTree[index];
+        int leftChild = findMin(index * 2 + 1, start, (start + end) / 2);
+        int rightChild = findMin(index * 2 + 2, (start + end) / 2 + 1, end);
+        return values[leftChild] > values[rightChild] ? rightChild : leftChild;
+    }
+
+    private static long max(long a, long b, long c) {
+        return a > b ? (a > c ? a : c) : b > c ? b : c;
+    }
 }
+
+
+
